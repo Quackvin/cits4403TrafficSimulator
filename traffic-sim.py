7a80b95b85b393acefa4cbd1b29dc574cd78207f
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-05-23 23:26:19 +0800

    consolidate accelerate

--- traffic-sim.py
+++ traffic-sim.py
@@ -32,7 +32,7 @@ show_road_start = False
 show_car1 = True
 disable_turning = True
 slow_ints = False
-use_acc = True
+use_acc = False
 
 ratio1 = float(1)
 ratio2 = float(1)
@@ -575,11 +575,8 @@ class Car():
     def accelerate(self):
         # accelerate faster when going faster
         # how fast they accelerate dependant on car
-        if use_acc:
-            acceleration = (self.max_acceleration / 2) * self.read_speed() * self.read_speed() + 0.03
-            self.write_acc(acceleration)
-        else:
-            acceleration = (self.max_acceleration / 3) * self.read_speed() * self.read_speed() + 0.5
+        acceleration = (self.max_acceleration / 2) * self.read_speed() * self.read_speed() + 0.03
+        self.write_acc(acceleration)
 
         if acceleration > self.max_acceleration:
             acceleration = self.max_acceleration
